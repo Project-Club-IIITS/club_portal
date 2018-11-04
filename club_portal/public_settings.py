8d9b6f26a440be5ef800ca6f4c1f33612b695bee
coder-dude <coderdude1999@gmail.com>
2018-11-04 23:28:09 +0530

    Started Initial project

---
+++ club_portal/public_settings.py
@@ -0,0 +1,16 @@
+import os
+
+IS_PUBLIC = True
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'bm+!!um=2p&r^!4(l5bpic2f&a7h0!vk^zl$53@drcuvaps08+'
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
