c049274488e38d2a4ddb91e918a4a3bebacbdc7a
coder-dude <coderdude1999@gmail.com>
2018-11-07 02:25:03 +0530

    setup static media urls

--- club_portal/urls.py
+++ club_portal/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from base import views as base_views
@@ -24,3 +26,6 @@ urlpatterns = [
     path('posts/', include("posts.urls")),
     path('', base_views.IndexView.as_view(), name="index")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
