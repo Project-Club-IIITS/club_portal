aaed3e4ecadf54bf09bb620515e5f586afb1f0cb
coder-dude <coderdude1999@gmail.com>
2018-11-06 23:33:57 +0530

    Created basic apps

--- club_portal/settings.py
+++ club_portal/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'registration',
+    'accounts',
+    'posts'
 ]
 
 MIDDLEWARE = [
