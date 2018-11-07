efd6f4a5ddd7d359af47e65abbb6601f672eb4b2
jiteshm17 <33426178+jiteshm17@users.noreply.github.com>
2018-11-07 14:43:14 +0530

    Created register models

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_no = models.IntegerField()
+    batch = models.CharField(max_length=150)
+    roll_no = models.IntegerField()
+    profile_pic = models.ImageField()
+    status = models.CharField(max_length=150)
+    following_clubs = models.ManyToManyField(User)
+
+
+class GoogleAuth(models.Model):
+    google_id = models.URLField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+
+class EmailConfirmation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_confirmed = models.BooleanField(default=False)
