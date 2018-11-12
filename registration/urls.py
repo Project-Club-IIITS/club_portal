e5200207b5f2a6acea8c9571a0b0d41ed5cdc704
jayendrakantipudi <jayendra.k17@iiits.in>
2018-11-12 19:08:05 +0530

    Added Frontend for Login & Signup

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 app_name = "registration"
+from registration import views
 
 urlpatterns = [
-    
+	path('login/', views.login,name='login'),
+	path('signup/', views.signup,name='signup'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+	return render(request,'registration/login.html',{})
+
+def signup(request):
+	return render(request,'registration/signup.html',{})
+
