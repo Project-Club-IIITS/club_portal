1eaeb862bbdd05e47171474cd5a449b32405ffa9
vismith24 <vismith.24.adappa@gmail.com>
2018-11-13 20:19:38 +0530

    added basic password reset

--- club_portal/settings.py
+++ club_portal/settings.py
@@ -110,14 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.conf.urls import url
 app_name = "registration"
 
 urlpatterns = [
-    
+    url('^', include('django.contrib.auth.urls')),
 ]
