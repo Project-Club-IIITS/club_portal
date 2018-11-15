7d65e497163aee98805f0490fad3883d68a3e84f
vismith24 <vismith.24.adappa@gmail.com>
2018-11-16 02:00:09 +0530

    Email Conf (Update it according to the models and forms)

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from django.conf.urls import url
+from . import views
 app_name = "registration"
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
