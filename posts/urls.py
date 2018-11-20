f2f9bdc1096258da611b7f355e69927a97e42cd9
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-20 16:33:07 +0530

    modified urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "posts"
 
-urlpatterns = [
-    
-]
+urlpatterns = {
+    path('', views.posts, name="posts"),
+    path('post_detail/', views.post_detail, name='post_detail'),
+
+}
