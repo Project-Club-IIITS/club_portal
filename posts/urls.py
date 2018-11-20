865de9250214c1562de1f4e387118bf00a16318b
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-20 17:14:54 +0530

    modified urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 app_name = "posts"
 
-urlpatterns = {
+urlpatterns = [
     path('', views.posts, name="posts"),
     path('post_detail/', views.post_detail, name='post_detail'),
-
-}
+]
