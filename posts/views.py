ba9c5fd7cf98c1b11a0b68564177f1211432a14e
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-11-20 16:33:41 +0530

    modified views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def posts(request):
+    return render(request, "posts/posts.html")
+
+
+def post_detail(request):
+    return render(request, "posts/post_detail.html")
