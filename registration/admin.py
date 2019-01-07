0ffff539bbffb3153b5c3f7f246e8443a35aa253
adwait-thattey <coderdude1999@gmail.com>
2019-01-07 16:44:25 +0530

    setup jwt token auth and views

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import EmailConfirmation
+from .models import EmailConfirmation, LastPasswordChange
 
 
 admin.site.register(EmailConfirmation)
+admin.site.register(LastPasswordChange)
