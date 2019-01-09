84c9ac616dfb584f761cca1cb1d4d54c1ec5971a
adwait-thattey <coderdude1999@gmail.com>
2019-01-09 22:17:36 +0530

    modify base admin

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Club
+from .models import Club, ClubMember, ClubModerator, ClubPresident
 # Register your models here.
 
 admin.site.register(Club)
+admin.site.register(ClubModerator)
+admin.site.register(ClubMember)
+admin.site.register(ClubPresident)
+
