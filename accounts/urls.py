d27d4f8eae44369cc588d415f62dd59f40edc8b1
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-01-10 19:22:47 +0530

    add moderator and member

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<slug:club_name>/list-for-president/', views.listForPresident, name='list_for_president'),
     path('<slug:club_name>/list-for-moderator/', views.listForModerator, name='list_for_moderator'),
     path('makeMember/', views.makeMember, name='make_member'),
+    path('makeModerator/', views.makeModerator, name='make_moderator'),
 ]
