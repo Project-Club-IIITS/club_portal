ae3517e34f1e8a42ab7ac11312613253b0c385e0
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-01-16 00:01:36 +0530

    Modified urls.py

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,10 @@ app_name = "base"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="base/index.html"), name='index'),
-    path('user-list', views.usersList, name='user_list'),
+    path('user-list/', views.usersList, name='user_list'),
     path('clubs/', views.club_home, name='club-description'),
-    path('admin/', views.admin_home, name='admin'),
+    path('member_list/', views.member_list, name='member_list'),
+    path('moderator_list/', views.moderator_list, name='moderator_list'),
+    path('pending_posts_list/', views.pending_posts_list, name='pending_posts_list'),
+
 ]
