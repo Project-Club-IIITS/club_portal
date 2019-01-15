36acdde1c279836449e003f8cf63fb4dc5e8447c
adwait-thattey <coderdude1999@gmail.com>
2019-01-15 19:09:13 +0530

    add calendatr

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from accounts.models import Calendar
+
 
 class PasswordResetForm(forms.Form):
     old_password = forms.CharField(widget=forms.PasswordInput, required=False)
@@ -11,3 +13,32 @@ class PasswordResetForm(forms.Form):
 
         if cleaned_data['new_password'] != cleaned_data['new_password2']:
             self.add_error('new_password2', 'Both Passwords do not match!')
+
+
+MONTHS = (
+    (1, 'January'),
+    (2, 'February'),
+    (3, 'March'),
+    (4, 'April'),
+    (5, 'May'),
+    (6, 'June'),
+    (7, 'July'),
+    (8, 'August'),
+    (9, 'September'),
+    (10, 'October'),
+    (11, 'November'),
+    (12, 'December'),
+)
+
+
+class CalendarForm(forms.ModelForm):
+    class Meta:
+        model = Calendar
+        fields = (
+            'title',
+        )
+
+
+class ChooseMonthAndYearForm(forms.Form):
+    month = forms.ChoiceField(choices=MONTHS)
+    year = forms.ChoiceField(choices=[(x, x) for x in range(1950, 3000)], initial=2019)
--- calendar/put_calendar/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from put_calendar.views import check_date
-# Register your models here.
-admin.site.register(check_date)
