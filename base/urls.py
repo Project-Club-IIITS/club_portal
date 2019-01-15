05da6fd1f46405522db0e99261f68001133748e3
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-01-15 12:15:39 +0530

    added urls

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,8 @@ from django.contrib.auth.views import TemplateView
 app_name = "base"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = "base/index.html"), name='index'),
+    path('', TemplateView.as_view(template_name="base/index.html"), name='index'),
     path('user-list', views.usersList, name='user_list'),
     path('clubs/', views.club_home, name='club-description'),
+    path('admin/', views.admin_home, name='admin'),
 ]
