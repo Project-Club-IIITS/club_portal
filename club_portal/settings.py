1a1d6232dd0f05536e10ed6d2774ee03095a8bfa
adwait-thattey <coderdude1999@gmail.com>
2019-01-28 17:04:49 +0530

    final changes

--- club_portal/settings.py
+++ club_portal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = secret_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = secret_settings.ALLOWED_HOSTS
+ALLOWED_HOSTS = ['*']
 
 if DEBUG is False and secret_settings.IS_PUBLIC is True:
     raise ImportError(
